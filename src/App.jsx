79665b52b9b6ff9212315d1d976ab44a0bcb1820
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-21 17:34:09 +0330

    add Product route

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 //Layout
 import Layout from "./layouts/Layout";
+
+//pages
 import Products from "./pages/Products";
+import Product from "./pages/Product";
 
 function App() {
   return (
@@ -11,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/products" />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<Product />} />
       </Routes>
     </Layout>
   );
