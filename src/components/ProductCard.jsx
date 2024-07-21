b594c4c0589af4011c1afc5d9edc195b0152bd34
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-21 17:35:06 +0330

    add Link to product details

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,3 +1,6 @@
+//react-router-dom
+import { Link } from "react-router-dom";
+
 //context
 import { useContext } from "react";
 import { ProductsProvider } from "../pages/Products";
@@ -26,7 +29,9 @@ function ProductCard() {
           <div className="font-semibold ml-5 mt-3">${product.price}</div>
           <div className="flex justify-between items-center mx-5 mt-7 mb-3 text-2xl text-orange-500">
             <div>
-              <TbListDetails />
+              <Link to={`${product.id}`}>
+                <TbListDetails />
+              </Link>
             </div>
             <div className="bg-orange-500 text-white rounded-lg p-1">
               <TbShoppingBagCheck />
