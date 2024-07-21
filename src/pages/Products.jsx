bc290cfcd69f2073978d24ac382f76cd789e07b1
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-21 16:21:27 +0330

    add context & SearchBox

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,10 +1,14 @@
-import { useEffect, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 //services
 import products_api from "../services/config";
 
 //components
 import ProductCard from "../components/ProductCard";
+import SearchBox from "../components/SearchBox";
+import Categories from "../components/Categories";
+
+export const ProductsProvider = createContext();
 
 function Products() {
   const [products, setProducts] = useState([]);
@@ -12,11 +16,15 @@ function Products() {
     products_api.get().then((res) => setProducts(res));
   }, []);
   return (
-    <div className="grid grid-cols-3 gap-x-20 gap-y-10 my-10">
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </div>
+    <ProductsProvider.Provider value={{ products, setProducts }}>
+      <SearchBox />
+      <div className="grid grid-cols-11 gap-x-5 my-10">
+        <div className="col-span-9 grid grid-cols-12 gap-5">
+          <ProductCard />
+        </div>
+        <Categories />
+      </div>
+    </ProductsProvider.Provider>
   );
 }
 
