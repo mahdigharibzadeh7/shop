d70906eabca73af9f33e209095bbf461ae333dd3
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-21 14:11:08 +0330

    add some css

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <div className="text-center text-lg bg-orange-500 px-7 py-5 rounded-lg mt-10 mb-2">
+    <div className="text-center text-lg bg-orange-500 text-white px-7 py-5 rounded-lg mt-10 mb-2">
       Developed by
       <span className="text-orange-900 px-2 py-1 rounded-lg">
         Mahdi Gharibzadeh
--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -3,7 +3,7 @@ import { FaCartShopping } from "react-icons/fa6";
 
 function Header() {
   return (
-    <div className="flex justify-between items-center bg-orange-500 px-7 py-5 rounded-lg mt-2">
+    <div className="flex justify-between items-center bg-orange-500 text-white px-7 py-5 rounded-lg mt-2">
       <div className="font-bold text-2xl">Shop</div>
       <div className="text-2xl">
         <FaCartShopping />
