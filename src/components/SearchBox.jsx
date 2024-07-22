298f1986991de014cff7914a99a86c1dd12f4bc4
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-22 11:58:08 +0330

    fix searchHandler to search products

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,14 +1,30 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+
+//context
+import { ProductsProvider } from "../pages/Products";
 
 //icons
 import { IoSearchSharp } from "react-icons/io5";
+import products_api from "../services/config";
 
 function SearchBox() {
   const [search, setSearch] = useState("");
+
+  const { setProducts } = useContext(ProductsProvider);
+
   const searchHandler = () => {
-    console.log(search);
+    if (search) {
+      products_api.get().then((res) => {
+        const newProducts = res.filter((product) =>
+          product.title.toLowerCase().includes(search)
+        );
+        setProducts(newProducts);
+      });
+    } else products_api.get().then((res) => setProducts(res));
+
     setSearch("");
   };
+
   return (
     <div className="flex items- gap-x-3 mt-10">
       <input
@@ -16,7 +32,7 @@ function SearchBox() {
         type="text"
         placeholder="Search..."
         value={search}
-        onChange={(e) => setSearch(e.target.value)}
+        onChange={(e) => setSearch(e.target.value.toLowerCase())}
       />
       <button
         className="bg-orange-500 p-2 text-white text-xl rounded-lg"
