84e11863ab120ff7dadee6c4ac0474317ff827d7
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-07-22 14:08:35 +0330

    add buy option

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,16 +1,27 @@
+import { useContext, useState } from "react";
+
 //react-router-dom
 import { Link } from "react-router-dom";
 
 //context
-import { useContext } from "react";
 import { ProductsProvider } from "../pages/Products";
 
 //icons
 import { TbListDetails } from "react-icons/tb";
 import { TbShoppingBagCheck } from "react-icons/tb";
+import { BsTrash } from "react-icons/bs";
 
 function ProductCard() {
+  const [isShop, setIsShop] = useState(false);
+  const [count, setCount] = useState(0);
+
   const { products } = useContext(ProductsProvider);
+
+  const shopHandler = () => {
+    setCount(1);
+    setIsShop(true);
+  };
+
   return (
     <>
       {products.map((product) => (
@@ -27,15 +38,40 @@ function ProductCard() {
             {product.title}
           </div>
           <div className="font-semibold ml-5 mt-3">${product.price}</div>
-          <div className="flex justify-between items-center mx-5 mt-7 mb-3 text-2xl text-orange-500">
+          <div className="flex justify-between items-center mx-5 mt-7 mb-5 text-2xl text-orange-500">
             <div>
               <Link to={`${product.id}`}>
                 <TbListDetails />
               </Link>
             </div>
-            <div className="bg-orange-500 text-white rounded-lg p-1">
-              <TbShoppingBagCheck />
-            </div>
+            {isShop ? (
+              <div className="flex items-center">
+                <button
+                  className="flex items-center justify-center bg-orange-500 rounded-lg text-white w-7 h-7 text-lg"
+                  onClick={() => setCount((count) => count - 1)}
+                >
+                  {count === 1 ? (
+                    <BsTrash onClick={() => setIsShop(false)} />
+                  ) : (
+                    "-"
+                  )}
+                </button>
+                <span className="mx-5 text-black text-lg">{count}</span>
+                <button
+                  className="flex items-center justify-center bg-orange-500 rounded-lg text-white w-7 h-7 text-lg"
+                  onClick={() => setCount((count) => count + 1)}
+                >
+                  +
+                </button>
+              </div>
+            ) : (
+              <div className="bg-orange-500 text-white rounded-lg p-1">
+                <TbShoppingBagCheck
+                  className="cursor-pointer"
+                  onClick={shopHandler}
+                />
+              </div>
+            )}
           </div>
         </div>
       ))}
