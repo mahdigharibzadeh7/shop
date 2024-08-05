11151995c58a6ac3f7091e9ae38aa7eb6df59759
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-08-05 16:21:26 +0330

    add 404 and ProductProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 //react-router-dom
 import { Routes, Route, Navigate } from "react-router-dom";
 
@@ -8,16 +10,26 @@ import Layout from "./layouts/Layout";
 import Products from "./pages/Products";
 import ProductDetails from "./pages/ProductDetails";
 import CheckOut from "./pages/CheckOut";
+import PageNotFound from "./pages/404";
+
+//context
+import ProductProvider from "./contexts/ProductContext";
 
 function App() {
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<Navigate to="/products" />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:id" element={<ProductDetails />} />
-        <Route path="/checkout" element={<CheckOut />} />
-      </Routes>
+      <ProductProvider>
+        <Routes>
+          <Route index element={<Navigate to="/products" replace />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+          {/* <Route
+            path="/checkout"
+            element={<CheckOut totalCount={totalCount} />}
+          /> */}
+          <Route path="/*" element={<PageNotFound />} />
+        </Routes>
+      </ProductProvider>
     </Layout>
   );
 }
