b62f044c86cb30130b23d57ba486221eee59c218
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-08-05 16:29:39 +0330

    ProductProvider

---
+++ src/contexts/ProductContext.jsx
@@ -0,0 +1,37 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+//services
+import products_api from "../services/config";
+
+//context
+const ProductContext = createContext();
+
+function ProductProvider({ children }) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await products_api.get();
+        setProducts(res);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <ProductContext.Provider value={products}>
+      {children}
+    </ProductContext.Provider>
+  );
+}
+
+const useProduct = () => {
+  const products = useContext(ProductContext);
+  return products;
+};
+
+export default ProductProvider;
+export { useProduct };
