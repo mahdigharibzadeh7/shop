cfbdaeae2c6b5a6979146e9bddce8fb2ede3ce53
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-08-06 23:05:16 +0330

    add useCart and a message for empty cart

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -1,13 +1,25 @@
+//context
+import { useCart } from "../contexts/CartContext";
+
 //components
 import CheckOutFactor from "../components/CheckOutFactor";
 import CheckOutList from "../components/CheckOutList";
 
 function CheckOut() {
+  const [state] = useCart();
   return (
-    <div className="grid grid-cols-12 mt-20">
-      <CheckOutFactor />
-      <CheckOutList />
-    </div>
+    <>
+      {state.itemsCounter === 0 ? (
+        <div className="text-center my-20 font-bold text-xl">
+          There is no product in your cart yet!
+        </div>
+      ) : (
+        <div className="grid grid-cols-12 mt-20">
+          <CheckOutFactor />
+          <CheckOutList />
+        </div>
+      )}
+    </>
   );
 }
 
