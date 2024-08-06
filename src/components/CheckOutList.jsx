2c553376fec0c96703f964113b97333c669e7e7d
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-08-06 23:01:13 +0330

    add useCart and ShopButtons component

--- src/components/CheckOutList.jsx
+++ src/components/CheckOutList.jsx
@@ -1,34 +1,45 @@
-import { useEffect, useState } from "react";
-import products_api from "../services/config";
+import { useState } from "react";
+
+//components
+import ShopButtons from "./ShopButtons";
+
+//context
+import { useCart } from "../contexts/CartContext";
+
+//helpers
+import { shortenText } from "../helpers/helper";
 
 function CheckOutList() {
-  const [product, setProduct] = useState([]);
-  useEffect(() => {
-    products_api.get().then((res) => setProduct(res));
-  }, []);
+  const [count, setCount] = useState(0);
+  const [isShop, setIsShop] = useState(false);
+
+  const [state, dispatch] = useCart();
 
   return (
     <div className="col-span-9">
-      {product.map((p) => (
+      {state.selectedItems.map((product) => (
         <div
-          key={p.id}
+          key={product.id}
           className="flex justify-between items-center px-5 py-7 my-3 border-2 border-dashed border-zinc-300 rounded-2xl"
         >
           <div>
-            <img className="w-12 h-12" src={p.image} alt={p.title} />
+            <img
+              className="w-12 h-12"
+              src={product.image}
+              alt={product.title}
+            />
           </div>
           <div className="text-ellipsis overflow-hidden whitespace-nowrap w-48">
-            {p.title}
+            {shortenText(product.title)}
           </div>
-          <div className="flex items-center">
-            <button className="flex items-center justify-center bg-mainBgColor rounded-lg text-mainTxtColor w-7 h-7 text-lg">
-              -
-            </button>
-            <span className="mx-3 text-black text-lg">1</span>
-            <button className="flex items-center justify-center bg-mainBgColor rounded-lg text-mainTxtColor w-7 h-7 text-lg">
-              +
-            </button>
-          </div>
+          <ShopButtons
+            count={product.quantity}
+            setCount={setCount}
+            setIsShop={setIsShop}
+            dispatch={dispatch}
+            rating={product.rating}
+            product={product}
+          />
         </div>
       ))}
     </div>
