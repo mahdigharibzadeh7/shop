0bb6a7c499d62f183104c761715839d63ecacd10
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-08-06 11:11:39 +0330

    fix response interceptors

--- src/services/config.js
+++ src/services/config.js
@@ -4,20 +4,16 @@ const products_api = axios.create({
   baseURL: "https://fakestoreapi.com/products",
 });
 
-products_api.interceptors.request.use((req) => {
-  return req;
-});
-
-products_api.interceptors.response.use((res) => {
-  return res.data;
-});
+products_api.interceptors.response.use(
+  (res) => res.data,
+  (error) => Promise.reject(error)
+);
 
 const getProductById = (id) => products_api.get(`/${id}`);
 
 const getCategories = () => products_api.get("/categories");
 
-const getProductsByCategory = (category) =>
-  products_api.get(`/category/${category}`);
+
 
 export default products_api;
-export { getProductById, getCategories, getProductsByCategory };
+export { getProductById, getCategories };
