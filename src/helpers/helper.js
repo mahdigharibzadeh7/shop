56836090c8394352df95c49c7ddca4a76259f041
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-08-06 13:45:36 +0330

    create 3 function: capitalization, createQueryObject, initialQuery

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -1,5 +1,12 @@
 const shortenText = (text) => text.split(" ").slice(0, 3).join(" ");
 
+const categoryCapitalization = (category) => {
+  return category
+    .split(" ")
+    .map((cat) => cat[0].toUpperCase() + cat.slice(1))
+    .join(" ");
+};
+
 const searchProducts = (products, search) => {
   if (search) {
     const searchedProducts = products.filter((product) =>
@@ -13,10 +20,37 @@ const filterProducts = (products, category) => {
   if (category) {
     const filteredProducts = products.filter(
       (product) => product.category === category
-      );
-      console.log(filteredProducts)
+    );
     return filteredProducts;
   } else return products;
 };
 
-export { shortenText, searchProducts, filterProducts };
+const createQueryObject = (currentQuery, newQuery) => {
+  if (newQuery.category === "all") {
+    const { category, ...rest } = currentQuery;
+    return rest;
+  }
+  if (newQuery.search === "") {
+    const { search, ...rest } = currentQuery;
+    return rest;
+  }
+  return { ...currentQuery, ...newQuery };
+};
+
+const getInitialQuery = (searchParams) => {
+  const query = {};
+  const category = searchParams.get("category");
+  const search = searchParams.get("search");
+  if (category) query.category = category;
+  if (search) query.search = search;
+  return query;
+};
+
+export {
+  shortenText,
+  categoryCapitalization,
+  searchProducts,
+  filterProducts,
+  createQueryObject,
+  getInitialQuery,
+};
