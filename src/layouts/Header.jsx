266e80bb5ac9094a4b886eb8f3e239383ae3d6e4
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-08-06 11:09:03 +0330

    fix Link

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <div className="flex justify-between items-center bg-mainBgColor text-mainTxtColor px-7 py-5 rounded-lg mt-2">
       <div className="font-bold text-2xl">
-        <Link to="/">Shop</Link>
+        <Link to="/products">Shop</Link>
       </div>
       <div className="text-2xl">
         <Link to="/checkout">
