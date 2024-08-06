87c6d0e5f67fa2018945f20325ffd1d31e305ba1
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-08-06 23:01:51 +0330

    add useCart, ShopButton component, state and dispatch

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,20 +1,33 @@
 import { useState } from "react";
 
+//components
+import ShopButtons from "./ShopButtons";
+
 //react-router-dom
 import { Link } from "react-router-dom";
+
+//helpers
+import { shortenText } from "../helpers/helper";
+
+//context
+import { useCart } from "../contexts/CartContext";
 
 //icons
 import { TbListDetails } from "react-icons/tb";
 import { TbShoppingBagCheck } from "react-icons/tb";
-import { BsTrash } from "react-icons/bs";
 
-function ProductCard({ product: { id, image, title, price } }) {
+function ProductCard({ product }) {
   const [isShop, setIsShop] = useState(false);
   const [count, setCount] = useState(0);
 
+  const { id, image, title, price, rating } = product;
+
+  const [, dispatch] = useCart();
+
   const shopHandler = () => {
-    setCount(1);
     setIsShop(true);
+    setCount(1);
+    dispatch({ type: "FIRST_BUY", payload: product });
   };
 
   return (
@@ -23,8 +36,8 @@ function ProductCard({ product: { id, image, title, price } }) {
       className="col-span-4 border-2 border-dashed border-zinc-300 rounded-xl bg-white"
     >
       <img className="w-36 h-40 mx-auto my-5" src={image} alt={title} />
-      <div className="text-red-500 font-semibold ml-5 text-ellipsis overflow-hidden whitespace-nowrap mx-5">
-        {title}
+      <div className="text-red-500 font-semibold ml-5 mx-5">
+        {shortenText(title)}
       </div>
       <div className="font-semibold ml-5 mt-3">${price}</div>
       <div className="flex justify-between items-center mx-5 mt-7 mb-5 text-2xl text-mainBgColor">
@@ -34,21 +47,14 @@ function ProductCard({ product: { id, image, title, price } }) {
           </Link>
         </div>
         {isShop ? (
-          <div className="flex items-center">
-            <button
-              className="flex items-center justify-center bg-mainBgColor rounded-lg text-mainTxtColor w-7 h-7 text-lg"
-              onClick={() => setCount((count) => count - 1)}
-            >
-              {count === 1 ? <BsTrash onClick={() => setIsShop(false)} /> : "-"}
-            </button>
-            <span className="mx-5 text-black text-lg">{count}</span>
-            <button
-              className="flex items-center justify-center bg-mainBgColor rounded-lg text-mainTxtColor w-7 h-7 text-lg"
-              onClick={() => setCount((count) => count + 1)}
-            >
-              +
-            </button>
-          </div>
+          <ShopButtons
+            count={count}
+            setCount={setCount}
+            setIsShop={setIsShop}
+            dispatch={dispatch}
+            rating={rating}
+            product={product}
+          />
         ) : (
           <div className="bg-mainBgColor text-mainTxtColor rounded-lg p-1">
             <TbShoppingBagCheck
