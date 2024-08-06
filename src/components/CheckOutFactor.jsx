049cc9b8f1230468dfd464c6a0a76cb80c80cc30
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-08-06 23:00:37 +0330

    add useCart, state and dispatch

--- src/components/CheckOutFactor.jsx
+++ src/components/CheckOutFactor.jsx
@@ -1,25 +1,37 @@
+//context
+import { useCart } from "../contexts/CartContext";
+
 //icons
 import { GoChecklist } from "react-icons/go";
 import { LuBadgeCheck } from "react-icons/lu";
 import { FaHashtag } from "react-icons/fa";
 
 function CheckOutFactor() {
+  const [state, dispatch] = useCart();
+
+  const checkOutHandler = () => {
+    dispatch({ type: "CHECKOUT" });
+  };
+
   return (
     <div className="col-span-3 border-2 border-dashed border-mainBgColor rounded-xl p-5 w-64 h-fit text-center">
       <div className="flex items-center gap-x-1 text-mainBgColor text-lg font-semibold">
         <GoChecklist className="text-xl" />
-        Total:
+        Total Price: ${state.totalPrice}
       </div>
       <div className="flex items-center gap-x-1 text-mainBgColor text-lg font-semibold my-3">
         <FaHashtag className="text-xl" />
-        Quantity:
+        Quantity: {state.itemsCounter}
       </div>
       <div className="flex items-center gap-x-1 text-mainBgColor text-lg font-semibold">
         <LuBadgeCheck className="text-xl" />
-        Status:
+        Status: pending
       </div>
       <div>
-        <button className="bg-mainBgColor text-mainTxtColor w-52 mt-14 rounded-lg py-[0.1rem]">
+        <button
+          className="bg-mainBgColor text-mainTxtColor w-52 mt-14 rounded-lg py-[0.1rem]"
+          onClick={checkOutHandler}
+        >
           Checkout
         </button>
       </div>
