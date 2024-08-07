0a7db1ff43be613b992e7c6e93d59a7bef29700f
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-08-07 12:20:16 +0330

    create 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,9 +1,22 @@
+//react-router-dom
+import { Link } from "react-router-dom";
+
+//icons
+import { FaArrowLeftLong } from "react-icons/fa6";
+
 function PageNotFound() {
   return (
-    <div>
-      404
+    <div className="flex flex-col items-center my-20 font-bold text-2xl">
+      <div>Page Not Found!</div>
+      <div className="py-5">404</div>
+      <Link to="/products">
+        <button className="flex items-center gap-x-2 bg-mainBgColor text-mainTxtColor px-3 py-1 rounded-lg text-lg">
+          <FaArrowLeftLong />
+          Back to Shop
+        </button>
+      </Link>
     </div>
-  )
+  );
 }
 
-export default PageNotFound
+export default PageNotFound;
