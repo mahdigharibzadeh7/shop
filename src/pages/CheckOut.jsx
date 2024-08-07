718ad73738bacce5c01037b20faf4de011751213
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-08-07 14:23:47 +0330

    add toast for successfull payment

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 //context
 import { useCart } from "../contexts/CartContext";
 
@@ -5,10 +7,21 @@ import { useCart } from "../contexts/CartContext";
 import CheckOutFactor from "../components/CheckOutFactor";
 import CheckOutList from "../components/CheckOutList";
 
+//toastify
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function CheckOut() {
   const [state] = useCart();
+
+  useEffect(() => {
+    if (state.checkOut)
+      toast.success("Your payment was successfully done!");
+  }, [state.checkOut]);
+
   return (
     <>
+      <ToastContainer />
       {state.itemsCounter === 0 ? (
         <div className="text-center my-20 font-bold text-xl">
           There is no product in your cart yet!
