5e10b6c5d5532cad28f663a2ee97bc521d8ec6a3
mahdigharibzadeh7 <gharibzadehmahdi7@gmail.com>
2024-08-07 13:12:45 +0330

    add useProductDetails

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
+//react-router-dom
 import { Link, useParams } from "react-router-dom";
 
-//services
-import { getProductById } from "../services/config";
+//context
+import { useProductDetails } from "../contexts/ProductContext";
 
 //components
 import Loader from "../components/Loader";
@@ -14,50 +14,44 @@ import { FaGg } from "react-icons/fa";
 
 function ProductDetails() {
   const { id } = useParams();
-  const [product, setProduct] = useState([]);
-  useEffect(() => {
-    getProductById(id).then((res) => setProduct(res));
-  }, []);
+
+  const product = useProductDetails(+id);
+  if (!product) return <Loader />;
+
   return (
-    <>
-      {product.id ? (
-        <div className="flex my-10">
-          <div className="border-2 border-dashed border-mainBgColor rounded-[3rem] min-w-72 h-fit bg-white">
-            <img
-              className="max-w-60 mx-auto my-5"
-              src={product.image}
-              alt={product.title}
-            />
+    <div className="flex my-10">
+      <div className="border-2 border-dashed border-mainBgColor rounded-[3rem] min-w-72 h-fit bg-white">
+        <img
+          className="max-w-60 mx-auto my-5"
+          src={product.image}
+          alt={product.title}
+        />
+      </div>
+      <div className="grid grid-cols-12 border-2 border-dashed border-zinc-300 rounded-[3rem] ml-10 p-7">
+        <div className="col-span-9 w-[80%]">
+          <div className="font-bold text-xl text-mainBgColor">
+            {product.title}
           </div>
-          <div className="grid grid-cols-12 border-2 border-dashed border-zinc-300 rounded-[3rem] ml-10 p-7">
-            <div className="col-span-9 w-[80%]">
-              <div className="font-bold text-xl text-mainBgColor">
-                {product.title}
-              </div>
-              <p className="my-7">{product.description}</p>
-              <div className="flex items-center gap-x-2 font-bold">
-                <FaGg className="text-mainBgColor text-lg" />
-                {product.category}
-              </div>
-              <div className="flex items-center gap-x-2 font-bold">
-                <IoMdPricetag className="text-mainBgColor text-lg" />$
-                {product.price}
-              </div>
-            </div>
-            <div className="col-span-3 content-end">
-              <Link to="/">
-                <button className="flex items-center gap-x-2 bg-mainBgColor text-mainTxtColor px-3 py-1 rounded-lg">
-                  <FaArrowLeftLong />
-                  Back to Shop
-                </button>
-              </Link>
-            </div>
+          <p className="my-7">{product.description}</p>
+          <div className="flex items-center gap-x-2 font-bold">
+            <FaGg className="text-mainBgColor text-lg" />
+            {product.category}
+          </div>
+          <div className="flex items-center gap-x-2 font-bold">
+            <IoMdPricetag className="text-mainBgColor text-lg" />$
+            {product.price}
           </div>
         </div>
-      ) : (
-        <Loader />
-      )}
-    </>
+        <div className="col-span-3 content-end">
+          <Link to="/">
+            <button className="flex items-center gap-x-2 bg-mainBgColor text-mainTxtColor px-3 py-1 rounded-lg">
+              <FaArrowLeftLong />
+              Back to Shop
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
 
